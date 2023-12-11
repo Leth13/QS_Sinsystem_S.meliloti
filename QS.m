37d8118ccc5c49840373a3b4dbae3ec2ddcc4057
Leth13 <142907459+Leth13@users.noreply.github.com>
2023-12-11 17:33:48 +0100

    Update QS.m

--- QS.m
+++ QS.m
@@ -25,13 +25,13 @@ epsilon_sinr = Parameters_From_File(14);
 rho = Parameters_From_File(15);
 
 
-%initial quantities of metabolites
+%initial quantities of metabolites or number of genes
 EXPR_init = 0;
 SINR_init = 0;
 SINI_init = 0;
 EXPRstar_init = 0;
 AHL_iinit = 0;
-AHL_oinit = 30;
+AHL_oinit = 0;
 DNAsinR_init = 1;
 DNAsinRstar_init = 0;
 DNAsinI_init = 1;
@@ -42,67 +42,19 @@ DNAExpR_init = 1;
 parameters =  [beta, alpha, deltaI, deltaO, mu, epsilon, k2, k1, k3, k4, k5, k6, k7, epsilon_sinr];
 
 Initials = [EXPR_init, SINR_init, SINI_init, EXPRstar_init, AHL_iinit, AHL_oinit, DNAsinRstar_init, DNAsinIstar_init, DNAsinR_init, DNAsinI_init, DNAExpR_init];
-[SINIplot, SINRplot, EXPRplot, timeplot, meantime] = SSA_QS_maxAHL(Initials, parameters, numberofcells, numberofreactions);    
+[SINIplot, SINRplot, EXPRplot, timeplot, meantime] = SSA_QS_maxAHL(Initials, parameters, numberofcells, numberofreactions); 
+
+%to start the deterministic model
 temp = [0 2500];
+% remove DNA_ExpR_init, it's an internal variable of the myode_full
 Initials(end) = [];
-%[tdet, det] = ode45(@(t,y) myode_full(y , parameters), temp, Initials);
-
-xxis = 2500;
-figure("name", "EXPR expression")
-set(gca,'Fontsize',18);
-for l = 1:numberofcells
-    plot(timeplot(:,l),EXPRplot(:,l), 'HandleVisibility','off');
-    hold on
-end
-hold on;
-%plot(tdet,det(:,1),'--k','Linewidth',4);
-xlabel('time [min]','interpreter','latex');
-ylabel('number of $EXPR$ molecules','interpreter','latex');
-hh=legend('solution of ODEs');
-set(hh,'interpreter','latex','Fontsize',18);
-yxis = max(max(EXPRplot))*1.007;
-axis([0 xxis 0 yxis])
-set(gca,'Fontsize',18);
+[tdet, det] = ode45(@(t,y) myode_full(y , parameters), temp, Initials);
 
 
-figure("name", "ExpRstar expression")
-set(gca,'Fontsize',18);
-for l = 1:numberofcells
-    plot(timeplot(:,l),EXPRstarplot(:,l), 'HandleVisibility','off');
-    hold on
-end
-hold on;
-%plot(tdet,det(:,4),'--k','Linewidth',4);
-xlabel('time [min]','interpreter','latex');
-ylabel('number of $EXPRstar$ molecules','interpreter','latex');
-hh=legend('solution of ODEs');
-set(hh,'interpreter','latex','Fontsize',18);
-%yxis = max(det(:,4))*1.007;
-axis([0 xxis 0 20])
-set(gca,'Fontsize',18);
-
-
-
-figure("name", "SINR expression")
-set(gca,'Fontsize',18);
-for l = 1:numberofcells
-    plot(timeplot(:,l),SINRplot(:,l),'HandleVisibility','off');
-    hold on
-end
-hold on;
-plot(tdet,det(:,2),'--k','Linewidth',4);
-xlabel('time [min]','interpreter','latex');
-ylabel('number of $SINR$ molecules','interpreter','latex');
-hh=legend('solution of ODEs');
-set(hh,'interpreter','latex','location','northwest','Fontsize',18);
-yxis = max(max(SINRplot))*1.007;
-axis([0 xxis 0 yxis])
-set(gca,'Fontsize',18);
+%visualizing the models
 
 figure("name", "SINI expression")
 set(gca,'Fontsize',18);
-set(gca,'Fontsize',18);
-set(hh,'interpreter','latex','location','northwest','Fontsize',18);
 for l = 1:numberofcells
     plot(timeplot(:,l),SINIplot(:,l),'HandleVisibility','off');
     hold on
@@ -112,28 +64,8 @@ plot(tdet,det(:,3),'--k','Linewidth',4);
 xlabel('time [min]','interpreter','latex');
 ylabel('number of $SINI$ molecules','interpreter','latex');
 hh=legend('solution of ODEs');
-%yxis = max(max(det(:,3)))*1.007;
+set(hh,'interpreter','latex','location','northwest','Fontsize',18);
+yxis = max(max(SINIplot(:,l)))*1.007;
 axis([0 xxis 0 yxis])
 set(gca,'Fontsize',18);
 
-[r,cell] = size(SINIplot);
-npeaks = size(cell);
-average_amplitude = size(cell);
-AllPulsesAmplitude = size(cell);
-for t = 1:1:cell
-    [peakLoc] = peakfinder(SINIplot(:,t), ((max(SINIplot(:,t))-min(SINIplot(:,t)))/4), 2);
-    %peakfinder(normalizedSinI(:,t));
-    time_of_peaks = timeplot(peakLoc,t);
-    values_of_peaks = SINIplot(peakLoc,t);
-    npeaks(t) = length(peakLoc);
-    average_amplitude(t) = mean(values_of_peaks);
-    AllPulsesAmplitude = [AllPulsesAmplitude values_of_peaks'];
-end
-pulses = (mean(npeaks)/(meantime))*60;
-
-
-%calculate total distance/time between peaks [per minute] and create a
-%vector with all values of distance
-meanmessage0 = "the mean time in hours between bursts of expression of SINI is";
-disp(meanmessage0)
-disp(pulses)
